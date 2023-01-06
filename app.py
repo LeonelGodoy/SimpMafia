82daa4a401a6bbe6c06d6ea277b02ac5b4464b86
Leonel <LeonelGodon@gmail.com>
2023-01-06 01:39:45 -0500

    WORKING

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    keys = {'MY_SECRET_SAUCE': 'onwbl5f7e4izyjd7h5x4yv8d4o6gat', 'MY_SECRET_ID': '0vx9sscucig7b24gyyrxjkjwj16azq'}
+    keys = {'MY_SECRET_SAUCE': os.environ['MY_SECRET_SAUCE'], 'MY_SECRET_ID': os.environ['MY_SECRET_SAUCE']}
     return render_template("index.html", keys=keys)
 
 if __name__ == '__main__':
