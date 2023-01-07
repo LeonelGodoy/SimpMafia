ff60b93d4a74c35d8b1ceef477869109070518ab
Leonel <LeonelGodon@gmail.com>
2023-01-06 19:43:22 -0500

    Website Working

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
 import os
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    print(os.environ['MY_SECRET_SAUCE'])
-    keys = {'MY_SECRET_SAUCE': os.environ['MY_SECRET_SAUCE'], 'MY_SECRET_ID': os.environ['MY_SECRET_SAUCE']}
-    return render_template("index.html", keys=keys)
+def main():  # put application's code here
+    client_id = os.environ['MY_SECRET_SAUCE']
+    client_secret = os.environ['MY_SECRET_ID']
+    headers = {
+        'Client-ID': client_id,
+        'client_secret': client_secret,
+        "grant_type": 'client_credentials',
+        'Authorization': 'Bearer ' + os.environ['MY_SECRET_TOKEN']
+    }
+    streamers = ['cm_nyc_tv', 'gamerpool474_ph', 'snoozefighting', 'scentless__apprentice', 'mattnguyen', 'leyopan']
+    dicts = {}
+    for streamer in streamers:
+        url = "https://api.twitch.tv/helix/streams?user_login=" + streamer
+
+        response = requests.get(url, headers=headers).json()
+        # data output
+        dicts[streamer] = len(response['data'])
+
+    return render_template("index.html", dicts=dicts)
+
 
 if __name__ == '__main__':
-    print(os.environ['MY_SECRET_SAUCE'])
     app.run()
--- static/js/twitch.js
+++ static/js/twitch.js
@@ -1,60 +1,18 @@
 
-function getTwitchAuthorization() {
-    let url = `https://id.twitch.tv/oauth2/token?client_id=${MY_SECRET_SAUCE}&client_secret=${MY_SECRET_ID}&grant_type=client_credentials`;
-
-    return fetch(url, {
-    method: "POST",
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        return data;
-    });
-}
-
-async function getStreams(username) {
-    const endpoint = "https://api.twitch.tv/helix/streams?user_login=" + username;
-
-    let authorizationObject = await getTwitchAuthorization();
-    let { access_token, expires_in, token_type } = authorizationObject;
-
-    token_type =
-    token_type.substring(0, 1).toUpperCase() +
-    token_type.substring(1, token_type.length);
-
-    let authorization = `${token_type} ${access_token}`;
-
-    let headers = {
-    authorization,
-    "Client-Id": MY_SECRET_SAUCE,
-    };
-
-    fetch(endpoint, {
-    headers,
-    })
-    .then((res) => res.json())
-    .then((data) => checkLIVE(data, username));
-
-}
 
 
-function checkLIVE(data, username) {
-    var { data: streams } = data;
-    if (streams.length == 0)
+
+
+setTimeout(function(){
+    //do what you need here
+
+
+Object.keys(parsed[0]).forEach(function(key) {
+    console.log('Key : ' + key + ', Value : ' + parsed[0][key])
+    if (parsed[0][key] === 1)
          {
-         document.getElementById(username).classList.remove("hidden");
-         }
-
-
-}
-
-
-function init(){
-    var array = document.getElementsByClassName("btn_live");
-    // regular for loop
-        for (let i = 0; i < array.length; i++) {
-            getStreams(array[i].name)
+         document.getElementById(key).classList.remove("hidden");
 
          }
-    }
 
-init()
+})}, 1000);
